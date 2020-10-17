f92299613030dc7c87533bd627ed1273c059de6e
Matheus <matheus.veras@lobofrontal.com.br>
2020-10-17 09:38:22 -0300

    Updates

---
+++ precache-manifest.01c6476ec9bb805e841c3547bbfc6f81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb09be7b885a3a06b0fa52a8e4f33a3f",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "1c97915854ea13e32001",
+    "url": "/resume/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5af71bb83c9e1aca4ac9",
+    "url": "/resume/static/js/2.293dbbd8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/resume/static/js/2.293dbbd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c97915854ea13e32001",
+    "url": "/resume/static/js/main.4a47b07f.chunk.js"
+  },
+  {
+    "revision": "8c1d914a1677cea53979",
+    "url": "/resume/static/js/runtime-main.75d8c91e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/resume/static/media/logo.5d5d9eef.svg"
+  }
+]);
